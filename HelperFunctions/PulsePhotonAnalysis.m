4fc98bb156e6ba6f106dce1a953b51a4e9925a88
David Gao <yhgao@mit.edu>
2022-06-01 20:28:15 -0400

    Add Photon distribution analysis for AWG

--- HelperFunctions/PulsePhotonAnalysis.m
+++ HelperFunctions/PulsePhotonAnalysis.m
@@ -1,47 +1,53 @@
-function time_bin = PulsePhotonAnalysis(sync_time, photon_time, draw_fig)
+function time_bin_result = PulsePhotonAnalysis(sync_time, photon_time, sync_ns, bin_width_ns, draw_fig)
 
     sync_pt = 1;
     photon_pt = 1;
-    % time_bin = cell(1, length(sync_time));
+    % time_bin_result = cell(1, length(sync_time));
     coordinate_time = zeros('like', photon_time);
     coordinate_round = zeros('like', photon_time);
 
     % for i = 1:length(sync_time)
-    % %     time_bin{i} = [];
+    % %     time_bin_result{i} = [];
     % end
     while sync_pt < length(sync_time)-1 && photon_pt < length(photon_time)
         while photon_time(photon_pt) < sync_time(sync_pt+1)
 
-            % time_bin{sync_pt} = [time_bin{sync_pt}, photon_time(photon_pt)-sync_time(sync_pt)];
+            % time_bin_result{sync_pt} = [time_bin_result{sync_pt}, photon_time(photon_pt)-sync_time(sync_pt)];
             coordinate_time(photon_pt) = photon_time(photon_pt)-sync_time(sync_pt);
             coordinate_round(photon_pt) = sync_pt;
             photon_pt = photon_pt + 1;
+            if photon_pt > length(photon_time)
+                break
+            end
         end
         sync_pt = sync_pt + 1;
     end
 
 
 
-
-    sync_ns = 10000;
+    if ~exist("sync_ns", "var")
+        sync_ns = 10000;
+    end
     % bin_width_ns = 0.256;
-    bin_width_ns = 1;
+    if ~exist("bin_width_ns", "var")
+        bin_width_ns = 1;
+    end
     bin_num = ceil(sync_ns/bin_width_ns);
 
-    time_bin = zeros(1, bin_num);
+    time_bin_result = zeros(1, bin_num);
 
     for photon_cnt = 1:length(coordinate_time)
         bin =ceil(mod(coordinate_time(photon_cnt)/1000, sync_ns)/bin_width_ns);
         if bin ~= 0
-        time_bin(bin) = time_bin(bin) + 1;
+        time_bin_result(bin) = time_bin_result(bin) + 1;
         end
     end
 
-    if exist("draw_fig", 'var')
+    if exist("draw_fig", 'var') && draw_fig
         figure;
         scatter(coordinate_time, coordinate_round)
         figure;
-        plot((1:bin_num)*bin_width_ns, time_bin)
+        plot((1:bin_num)*bin_width_ns, time_bin_result)
         set(get(gca, 'XLabel'), 'String', 'time (ns)');
         set(get(gca, 'YLabel'), 'String', 'Intensity (a.u.)');
         set(get(gca, 'Title'), 'String', sprintf("Resolution: %d ns", bin_width_ns));
--- Modules/+Experiments/AWGOptimize.m
+++ Modules/+Experiments/AWGOptimize.m
@@ -1,6 +1,4 @@
-classdef PicoHarp_tttr < Modules.Experiment
-    %Experimental wrapper for Drivers.PicoHarp300
-    
+classdef AWGOptimize < Modules.Experiment
     properties(SetObservable,AbortSet)
         picoharpH;
         data
@@ -8,24 +6,26 @@ classdef PicoHarp_tttr < Modules.Experiment
         PH_serialNr = 'No device';
         PH_BaseResolution = 'No device';
         connection = false;
-        Tacq_ms = 1000; %ms
+        Tacq_ms = 10000; %ms
         MaxTime_s = 3600*10; %s
         MaxCounts = 10000;
-        % SyncDivider = {uint8(1),uint8(2),uint8(4),uint8(8)};
         SyncDivider = uint8(1);
         SyncOffset = 0; %ms
         Ch0_CFDzero = 10;% mV
         Ch0_CFDlevel = 50;% mV
         Ch1_CFDzero = 10;% mV
         Ch1_CFDlevel = 50;% mV
-        % Binning = num2cell(0:7); % Binning can be 0 to MAXBINSTEPS-1 %time resolution = (PH_BaseResolution*2^Binning) ps
         Binning = 0;
         Offset = 0; %ms - still not sure what offset is this
         StopAtOverflow = true;
         OverflowCounts = 65535; %65535 is max value
         prefs = {'connection'};
+        sync_ns = 10000;
+        bin_width_ns = 10;
         PH_Mode         = 2; % 2 for T2 and 3 for T3 and 0 for histogram
-        show_prefs = {'PH_serialNr','PH_BaseResolution','connection','PH_Mode', 'MaxTime_s','MaxCounts','Binning','SyncDivider','SyncOffset','Ch0_CFDzero','Ch0_CFDlevel','Ch1_CFDzero','Ch1_CFDlevel','Tacq_ms','StopAtOverflow','OverflowCounts'};
+        SyncChannel = 0;
+        PhotonChannel = 1;
+        show_prefs = {'PH_serialNr','PH_BaseResolution','connection','PH_Mode', 'sync_ns', 'bin_width_ns', 'SyncChannel', 'PhotonChannel', 'Tacq_ms'};
         readonly_prefs = {'PH_serialNr','PH_BaseResolution'};
 
     end
@@ -36,25 +36,26 @@ classdef PicoHarp_tttr < Modules.Experiment
         acquiring = false;
     end
     methods(Access=private)
-        function obj = PicoHarp_tttr()
+        function obj = AWGOptimize()
             obj.loadPrefs;
         end
     end
+
     methods(Static)
         function obj = instance()
             mlock;
             persistent Object
             if isempty(Object) || ~isvalid(Object)
-                Object = Experiments.PicoHarp_tttr();
+                Object = Experiments.AWGOptimize();
             end
             obj = Object;
         end
     end
+
+
     methods
         function run(obj,status,managers,ax)
             assert(~isempty(obj.picoharpH)&&isvalid(obj.picoharpH),'PicoHarp driver not intialized properly.');
-            assert(isnumeric(obj.SyncDivider),'SyncDivider not set.')
-            assert(isnumeric(obj.Binning),'Binning not set.')
             status.String = 'Experiment started';
             obj.SetPHconfig;
             obj.data = [];
@@ -81,10 +82,11 @@ classdef PicoHarp_tttr < Modules.Experiment
             ofl_num = 0;
             cnt0 = 0;
             cnt1 = 0;
+            time_bin_result = zeros(1, ceil(obj.sync_ns/obj.bin_width_ns));
             while(ctcdone == 0 && obj.abort_request == false)
                 [buffer, nactual] = obj.picoharpH.PH_ReadFiFo;
-                % buffer(buffer == 4026531840) = 0;
-
+                cnt0_prev = cnt0;
+                cnt1_prev = cnt1;
                 for k = 1:nactual
                     chan = bitand(bitshift(buffer(k),-28),15);
                     cur_time_tag = bitand(buffer(k), 2^28-1);
@@ -92,22 +94,22 @@ classdef PicoHarp_tttr < Modules.Experiment
                         ofl_num = ofl_num + 1;
                     elseif (chan == 0)
                         cnt0 = cnt0 + 1;
-                        result0(cnt0) = double(ofl_num) * double(obj.picoharpH.WRAPAROUND) + double(cur_time_tag);
+                        result0(cnt0) = (double(ofl_num) * double(obj.picoharpH.WRAPAROUND) + double(cur_time_tag))*Resolution;
                     else % chan == 1
                         cnt1 = cnt1 + 1;
-                        result1(cnt1) = double(ofl_num) * double(obj.picoharpH.WRAPAROUND) + double(cur_time_tag);
+                        result1(cnt1) = (double(ofl_num) * double(obj.picoharpH.WRAPAROUND) + double(cur_time_tag))*Resolution;
                     end
                 end
 
-                    
                 if(nactual)
                     progress = progress + nactual;
                     status.String = sprintf('Progress: %9d, Time Elapsed: %0.2f\n',progress, toc(t));
-                    ax.Children(1).YData = result0(1:cnt0)*Resolution;
-                    ax.Children(1).XData = [1:cnt0];
-                    ax.Children(2).YData = result1(1:cnt1)*Resolution;
-                    ax.Children(2).XData = [1:cnt1];
-                    set(ax, 'XLim', [0,  max(cnt0, cnt1)])
+                    % time_bin_result = time_bin_result + PulsePhotonAnalysis(result0(cnt0_prev + 1:cnt0), result1(cnt1_prev + 1:cnt1), obj.sync_ns, obj.bin_width_ns);
+                    % time_bin_result = PulsePhotonAnalysis(result0(1:cnt0), result1(1:cnt1), obj.sync_ns, obj.bin_width_ns);
+                    ax.Children(1).YData = time_bin_result;
+                    ax.Children(1).XData = (1:ceil(obj.sync_ns/obj.bin_width_ns))*obj.bin_width_ns;
+                    set(ax, 'XLim', [0 obj.sync_ns])
+                    
                     drawnow limitrate;
                 else
                     ctcdone = int32(0);
@@ -115,31 +117,23 @@ classdef PicoHarp_tttr < Modules.Experiment
                     [ret, ctcdone] = calllib('PHlib', 'PH_CTCStatus', obj.picoharpH.DeviceNr, ctcdonePtr); 
                 end
             end
-            % [time_tags0, time_tags1] = obj.picoharpH.PH_GetTimeTags;
-            % ax.Children(2).YData = time_tags1;
-            % ax.Children(2).XData = 1:length(time_tags1);
-
-            % ax.Children(1).YData = time_tags0;
-            % ax.Children(1).XData = 1:length(time_tags0);
-            % drawnow limitrate;
             obj.picoharpH.PH_StopMeas;
-            obj.data.y0 = result0(1:cnt0);
-            obj.data.x0 = [1:cnt0];
-            obj.data.y1 = result1(1:cnt1);
-            obj.data.x1 = [1:cnt1];
+            obj.data.time_bin_result = time_bin_result;
+            obj.data.bin_width_ns = obj.bin_width_ns;
+            obj.data.sync_ns = obj.sync_ns;
             fprintf('\nDone\n');
         end
         
         function prepPlot(obj,ax)
             obj.data.x0 = [0];
             obj.data.y0 = [0];
-            obj.data.x1 = [0];
-            obj.data.y1 = [0];
-            plot(ax,obj.data.x0,obj.data.y0, obj.data.x1, obj.data.y1);
+            % obj.data.x1 = [0];
+            % obj.data.y1 = [0];
+            plot(ax,obj.data.x0,obj.data.y0);
             set(ax,'YLim',[0 inf])
             set(ax, 'XLim', [0 inf])
-            set(ax.XLabel,'String','Count')
-            set(ax.YLabel,'String','TimeStamp (ps)')
+            set(ax.XLabel,'String','Time (ns)')
+            set(ax.YLabel,'String','Counts')
         end
         
         function SetPHconfig(obj)
