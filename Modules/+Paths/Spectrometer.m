7a23114e6302ac295d1ce2e60401c0bda0930e8f
David Gao <yhgao@mit.edu>
2022-07-30 11:47:52 -0400

    Update name

--- Modules/+Paths/Spectrometer.m
+++ Modules/+Paths/Spectrometer.m
@@ -1,4 +1,4 @@
-function Spectrometer()
+function spectrometer()
     % Usage: In CommandCenter Menu, select Path->New Path, then edit the path name and copy the contents into the function body. This should be replaced by reading from code directly.
     try
         apt = Drivers.APTFilterFlipper.instance(37000231, 'spectrometer');
--- Modules/+Paths/Widefield.m
+++ Modules/+Paths/Widefield.m
@@ -1,4 +1,4 @@
-function Widefield()
+function widefield()
     % Usage: In CommandCenter Menu, select Path->New Path, then edit the path name and copy the contents into the function body. This should be replaced by reading from code directly.
     try
         sl2 = Drivers.Elliptec.Slider.instance("localhost", "CONFOCAL_WIDEFIELD_SLIDER");
