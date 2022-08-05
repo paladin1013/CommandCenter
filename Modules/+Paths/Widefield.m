97a468f21f053a89415dcea4daa34fd0cddd9630
David Gao <yhgao@mit.edu>
2022-08-04 20:19:53 -0400

    Change `WIDEFIELD_CONFOCAL_SLIDER` -> `CONFOCAL_WIDEFIELD_SLIDER`

--- Modules/+Paths/Widefield.m
+++ Modules/+Paths/Widefield.m
@@ -1,8 +1,8 @@
 function widefield()
     % Usage: In CommandCenter Menu, select Path->New Path, then edit the path name and copy the contents into the function body. This should be replaced by reading from code directly.
     try
-        sl2 = Drivers.Elliptec.Slider.instance("18.25.29.30", "CONFOCAL_WIDEFIELD_SLIDER");
-        sl2.set_position(2);
+        sl2 = Drivers.Elliptec.Slider.instance("18.25.29.30", "WIDEFIELD_CONFOCAL_SLIDER");
+        sl2.set_position(1);
     catch err
         warning("widefield path is not successfully set. Please turn off Kinesis or reconnect.");
     end
--- Modules/+Paths/confocal.m
+++ Modules/+Paths/confocal.m
@@ -1,8 +1,8 @@
 function confocal()
     % Usage: In CommandCenter Menu, select Path->New Path, then edit the path name and copy the contents into the function body. This should be replaced by reading from code directly.
     try
-        sl2 = Drivers.Elliptec.Slider.instance("18.25.29.30", "CONFOCAL_WIDEFIELD_SLIDER");
-        sl2.set_position(1);
+        sl2 = Drivers.Elliptec.Slider.instance("18.25.29.30", "WIDEFIELD_CONFOCAL_SLIDER");
+        sl2.set_position(2);
     catch err
         warning("APD_ZPL path is not successfully set. Please press `Ctrl-C` in the hwserver terminal.");
     end
